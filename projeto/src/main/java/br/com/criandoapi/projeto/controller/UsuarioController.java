fd6502433d928bc421dca18b629c2e464bc711af
caio-fernandiz <arostuche@gmail.com>
2024-10-29 07:42:01 -0300

    Projeto API

--- projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,4 +1,4 @@
-package br.com.criandoapi.projeto;
+package br.com.criandoapi.projeto.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
