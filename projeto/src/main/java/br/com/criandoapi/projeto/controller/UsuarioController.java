a8c6d0aed96b5b5891b7dfb1ce57bb5b05ddbf31
caio-fernandiz <arostuche@gmail.com>
2024-10-30 08:15:04 -0300

    Projeto API

--- projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package br.com.criandoapi.projeto.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,4 +41,12 @@ public class UsuarioController {
         Usuario usuarioNovo = dao.save(usuario);
         return usuarioNovo;
     }
+
+   @DeleteMapping("/{id}")
+   public Optional<Usuario> excluirUsuario(@PathVariable Integer id){
+    Optional<Usuario> usuario = dao.findById(id);
+    dao.deleteById(id);
+    return usuario;
+
+   }
 }
