c142b9689a5283db3d9f8f850cb00a43b9d1791b
caio-fernandiz <arostuche@gmail.com>
2024-10-30 07:58:11 -0300

    Projeto API

--- projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -4,20 +4,38 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.criandoapi.projeto.DAO.IUsuario;
 import br.com.criandoapi.projeto.model.Usuario;
 
+
+
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
     private IUsuario dao;
 
-    @GetMapping("/usuarios")
+    @GetMapping
     public List<Usuario> listaUsuarios(){
         return (List<Usuario>) dao.findAll();
     }
 
+    @PostMapping
+    public Usuario criarUsuario(@RequestBody Usuario usuario){
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+    
+    @PutMapping
+    public Usuario editarUsuario(@RequestBody Usuario usuario){
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
 }
